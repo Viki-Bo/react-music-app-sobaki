dd0142a4b126f05635268a27dbb7429a52364ca8
Viki-Bo <borodienko.v@gmail.com>
2021-08-11 06:26:15 -0700

    initial project music app

---
+++ src/Data.js
@@ -0,0 +1,94 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Beaver Creek",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Daylight",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+        name: "Zodiac",
+        cover: "https://chillhop.com/wp-content/uploads/2021/02/d12927eedcc2f5afba2ab049a4a1ea46c2266ae3-1024x1024.jpg",
+        artist: "C Y G N",
+        id: uuidv4(),
+        active: false,
+        color: ['#2D2734','#0C112E'],
+        audio:"https://mp3.chillhop.com/serve.php/?mp3=15000"
+    },
+    {
+        name:"Blue Moon",
+        cover: "https://chillhop.com/wp-content/uploads/2021/02/d12927eedcc2f5afba2ab049a4a1ea46c2266ae3-1024x1024.jpg",
+        artist: "C Y G N",
+        id: uuidv4(),
+        active: false,
+        color: ['#2D2734','#0D0B14'],
+        audio:"https://mp3.chillhop.com/serve.php/?mp3=14991",
+    },
+    {
+        name: "Bliss",
+        cover: "https://chillhop.com/wp-content/uploads/2020/09/5bff1a6f1bd0e2168d29b4c841b811598135e457-1024x1024.jpg",
+        artist:"Misha, Jussi Halme",
+        id: uuidv4(),
+        active: false,
+        color: ['#0E0E0E','#163150'],
+        audio:"https://mp3.chillhop.com/serve.php/?mp3=9248",
+    }
+  ];
+}
+
+export default chillHop;
---
+++ src/components/LibrarySong.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+const LibrarySong = ({song, songs, setCurrentSong, id, audioRef, isPlaying, setSongs}) => {
+
+    const songSelectHandler = async () => {
+        await setCurrentSong(song);
+
+        //active state
+        const newSongs = songs.map((song) => {
+            if(song.id === id){
+                return {
+                    ...song,
+                    active: true,
+                };
+            } else {
+                return {
+                    ...song,
+                    active: false,
+                };
+            }
+        });
+        setSongs(newSongs);
+
+        if (isPlaying) audioRef.current.play();
+    };
+
+    return(
+        <div 
+            onClick={songSelectHandler}
+            className={`librarys-song ${song.active ? 'selected' : ""}`}>
+            <img 
+                alt={song.name} 
+                src={song.cover}></img>
+                <div className="song-description">
+                    <h3>{song.name}</h3>
+                    <h4>{song.artist}</h4>
+                </div>
+        </div>
+    );
+};
+
+export default LibrarySong;
